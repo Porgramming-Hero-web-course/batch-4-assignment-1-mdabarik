8ce994cf5d50dee4b92669b7da4325376ed51d6a
mdabarik <mdabarik19@gmail.com>
2024-11-07 12:59:50 +0600

    problem-2.ts

---
+++ module/src/problem-2.ts
@@ -0,0 +1,24 @@
+{
+    // problem-2
+
+    const removeDuplicates = (nums: number[]): number[] => {
+        const uniqueNums : number[] = [];
+        for (let num of nums) {
+            if (!uniqueNums.includes(num)) {
+                uniqueNums.push(num);
+            }
+        }
+        return uniqueNums;
+    }
+    
+    
+    const arr : number[] = [1, 2, 2, 3, 4, 4, 5, 5, 5, 5, 5];
+    removeDuplicates(arr);
+
+
+
+
+
+    //
+}
+
