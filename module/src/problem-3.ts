deac1912e9369bc1d94334240a143276da89b874
mdabarik <mdabarik19@gmail.com>
2024-11-07 20:31:31 +0600

    problem-4.ts

--- module/src/problem-3.ts
+++ module/src/problem-3.ts
@@ -2,19 +2,21 @@
 {
     // start of block scope
     const countWordOccurrences = (str : string, key: string) : number => {
+        /*
+        1. convert str and key to lowercase.
+        2. remove special character from str and key using regular expression.
+        3. convert cleaned str into word space separated.
+        */
         const lowerCaseStr: string = str.toLowerCase();
-        const lowerCaseKey: string = key.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '');
-        const words: string[] = lowerCaseStr.replace(/[^a-zA-Z0-9\s]/g, '').split(" ");
-    
-        console.log(words[0]);
-        
+        const lowerCaseKey: string = key.toLowerCase().replace(/[^a-z0-9\s]/g, '');
+        const words: string[] = lowerCaseStr.replace(/[^a-z0-9\s]/g, '').split(" ");
+            
         let countFreqOfKey : number = 0;
         for (let i = 0; i < words.length; i++) {
             if (lowerCaseKey == words[i]) {
                 countFreqOfKey++;
             }
         }
-    
         return countFreqOfKey;
     }
     
