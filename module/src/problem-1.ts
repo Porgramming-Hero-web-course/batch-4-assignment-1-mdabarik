76d3f868ca26ea1141af84087a3ff837b1d06445
mdabarik <mdabarik19@gmail.com>
2024-11-07 11:20:56 +0600

    problem 1 solved

--- module/src/problem-1.ts
+++ module/src/problem-1.ts
@@ -1,1 +1,12 @@
-console.log('printing from problem-1.ts');
+// Problem 1
+
+const sumArray = (nums : number[]): number => {
+    let sum : number = 0;
+    for (let num of nums) {
+        sum += num;
+    }
+    return sum;
+}
+
+const arr : number[] = [1, 2, 3, 4, 5];
+console.log(sumArray(arr));
