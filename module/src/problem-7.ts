e8275861f463fd79c86110674b0c9ea3627f9ea1
mdabarik <mdabarik19@gmail.com>
2024-11-08 10:38:09 +0600

    problem-7.ts

---
+++ module/src/problem-7.ts
@@ -0,0 +1,30 @@
+{
+    //
+
+    type Vehicle = {
+        make: string;
+        model: string;
+        year: number;
+    }
+
+    class Car {
+        make: string;
+        model: string;
+        year: number;
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge() : number {
+            return 2024 - this.year;
+        }
+    }
+
+    const car = new Car("Honda", "Civic", 2018);
+    console.log(car.getCarAge());
+
+
+
+    //
+}
